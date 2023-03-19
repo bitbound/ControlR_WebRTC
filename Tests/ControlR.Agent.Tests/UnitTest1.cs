a1c5e10b99e338a399a0ea0e3ef765a7b2416c1d
Jared Goodwin <translucency@outlook.com>
2023-03-19 13:39:17 -0700

    Add another test project.

---
+++ Tests/ControlR.Agent.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using ControlR.Agent.Models.IpcDtos;
+using MessagePack;
+
+namespace ControlR.Agent.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+
+    }
+}
